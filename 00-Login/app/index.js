a25b6fffeb47e449d16d038c492d075be0cf51eb
Martin Walsh <martin.walsh@gmail.com>
2017-09-04 15:58:59 +0100

    Added ClearSession for Logout (#4)
    * Added ClearSession for Logout
    Ux Dynamic Logged in status
    
    * Use promise for clearSession
    Add more information to README
    
    * Update react-native react-native: 0.47.1
    
    * don't use clear session for Android

--- 00-Login/app/index.js
+++ 00-Login/app/index.js
@@ -9,9 +9,10 @@ import {
   Alert,
   AppRegistry,
   Button,
+  Platform,
   StyleSheet,
   Text,
-  View,
+  View
 } from 'react-native';
 import Auth0 from 'react-native-auth0';
 
@@ -19,29 +20,54 @@ var credentials = require('./auth0-credentials');
 const auth0 = new Auth0(credentials);
 
 export default class Auth0Sample extends Component {
-  _onLogin() {
-    auth0
-        .webAuth
-        .authorize({scope: 'openid email', audience: 'https://' + credentials.domain + '/userinfo'})
-        .then(credentials =>
-              Alert.alert(
-                  'Success',
-                  'AccessToken: ' + credentials.accessToken,
-                  [
-                    {text: 'OK', onPress: () => console.log('OK Pressed')},
-                  ],
-                  { cancelable: false }
-                ))
-        .catch(error => console.log(error));
+  constructor(props) {
+    super(props);
+    this.state = { accessToken: null };
   }
 
+  _onLogin = () => {
+    auth0.webAuth
+      .authorize({
+        scope: 'openid profile',
+        audience: 'https://' + credentials.domain + '/userinfo'
+      })
+      .then(credentials => {
+        Alert.alert(
+          'Success',
+          'AccessToken: ' + credentials.accessToken,
+          [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
+          { cancelable: false }
+        );
+        this.setState({ accessToken: credentials.accessToken });
+      })
+      .catch(error => console.log(error));
+  };
+
+  _onLogout = () => {
+    if (Platform.OS === 'android') {
+      this.setState({ accessToken: null });
+    } else {
+      auth0.webAuth
+        .clearSession({})
+        .then(success => {
+          this.setState({ accessToken: null });
+        })
+        .catch(error => console.log(error));
+    }
+  };
+
   render() {
+    let loggedIn = this.state.accessToken === null ? false : true;
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>
-          Auth0Sample - Login
+        <Text style={styles.header}>Auth0Sample - Login</Text>
+        <Text>
+          You are {loggedIn ? '' : 'not '}logged in.
         </Text>
-        <Button onPress={this._onLogin} title="Log In" />
+        <Button
+          onPress={loggedIn ? this._onLogout : this._onLogin}
+          title={loggedIn ? 'Log Out' : 'Log In'}
+        />
       </View>
     );
   }
@@ -52,13 +78,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   header: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
-  },
+    margin: 10
+  }
 });
 
 AppRegistry.registerComponent('Auth0Sample', () => Auth0Sample);

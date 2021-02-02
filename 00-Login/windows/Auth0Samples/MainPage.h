cc890e370634144cf22be60eadf9171c32f79418
Joao Matos <joao.matos@janeasystems.com>
2021-02-02 14:02:39 +0000

    Add Windows support

---
+++ 00-Login/windows/Auth0Samples/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::Auth0Samples::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::Auth0Samples::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+

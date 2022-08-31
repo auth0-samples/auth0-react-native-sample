e807f5559ebb5fbe7389af728099e0102f0e35e8
Poovamraj T T <poovamraj@gmail.com>
2022-08-31 18:26:30 +0200

    Sample with Hooks

--- 00-Login-Expo/App.js
+++ 00-Login-Expo/App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ import React from 'react';
+ import {Alert, Button, StyleSheet, Text, View} from 'react-native';
+ import {useAuth0, Auth0Provider} from 'react-native-auth0';
+ import config from './auth0-configuration';
+ 
+ const Home = () => {
+   const {authorize, clearSession, user, getCredentials} = useAuth0();
+ 
+   const onLogin = async () => {
+     try {
+       await authorize({scope: 'openid profile email'});
+       const {accessToken} = await getCredentials();
+       Alert.alert('AccessToken: ' + accessToken);
+     } catch (e) {
+       console.log(e);
+     }
+   };
+ 
+   const loggedIn = user !== undefined && user !== null;
+ 
+   const onLogout = async () => {
+     try {
+       await clearSession();
+     } catch (e) {
+       console.log('Log out cancelled');
+     }
+   };
+ 
+   return (
+     <View style={styles.container}>
+       <Text style={styles.header}> Auth0Sample - Login </Text>
+       {user && <Text>You are logged in as {user.name}</Text>}
+       {!user && <Text>You are not logged in</Text>}
+       <Button
+         onPress={loggedIn ? onLogout : onLogin}
+         title={loggedIn ? 'Log Out' : 'Log In'}
+       />
+     </View>
+   );
+ };
+ 
+ const App = () => {
+   return (
+     <Auth0Provider domain={config.domain} clientId={config.clientId}>
+       <Home />
+     </Auth0Provider>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     justifyContent: 'center',
+     alignItems: 'center',
+     backgroundColor: '#F5FCFF',
+   },
+   header: {
+     fontSize: 20,
+     textAlign: 'center',
+     margin: 10,
+   },
+ });
+ 
+ export default App;
+ 
